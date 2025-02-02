c77fe8f10d849a798293ee9887944b561f99b816
Rafael Tijero Fuentes <rtijero@contraloria.gob.pe>
2025-02-02 14:38:29 -0500

    Initial comit for backend

---
+++ src/test/java/app/rafo/bs_personal_finance_management/BsPersonalFinanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package app.rafo.bs_personal_finance_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsPersonalFinanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
